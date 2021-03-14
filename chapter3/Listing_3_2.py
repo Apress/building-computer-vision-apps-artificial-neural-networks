a1276120e77b02919797be033573909351e10148
Brian M Sheldon <21251328+BrianMSheldon@users.noreply.github.com>
2021-03-14 14:59:59 -0400

    Remove unused __future__ imports

--- chapter3/Listing_3_2.py
+++ chapter3/Listing_3_2.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import cv2
 import numpy as np
 
--- chapter5/label_data_creater.py
+++ chapter5/label_data_creater.py
@@ -1,4 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
 import tensorflow as tf
 import numpy as np
 import IPython.display as display
